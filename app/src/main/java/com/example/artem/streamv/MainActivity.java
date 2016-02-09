78491a2ad5d7718a0b9cbabbc2b70aec02fbe1a0
neek1337 <vdo1337@gmail.com>
2016-02-09 14:19:34 +0400

    asd

---
+++ app/src/main/java/com/example/artem/streamv/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.artem.streamv;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void HandleClick(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        switch (view.getId()) {
+            case R.id.button1:
+                intent.putExtra("streamerName", "altaoda");
+
+                break;
+            case R.id.button2:
+                // do stuff
+                break;
+        }
+        startActivity(intent);
+    }
+
+
+}
