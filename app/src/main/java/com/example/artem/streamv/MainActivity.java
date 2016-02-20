dd1270a5ff0502219ce9dca063766c12c85ad173
artem <arkuzyaev@gmail.com>
2016-02-21 00:19:38 +0400

    J{ET

--- app/src/main/java/com/example/artem/streamv/MainActivity.java
+++ app/src/main/java/com/example/artem/streamv/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_begin);
 
         Helper helper = new Helper(this);
 
@@ -31,6 +32,12 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
 
